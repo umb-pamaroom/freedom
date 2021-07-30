49907f17ac4dd56cc7bde3034be57f6a0863a8bc
umb-pamaroom <pamaroom@gmail.com>
2021-07-30 13:48:37 +0900

    My Django Girls app, first commit

---
+++ apps/notification/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notification(models.Model):
+    # 新規メッセージの場合
+    MESSAGE = 'message'
+    # 新規フォローの場合
+    FOLLOWER = 'follower'
+    LIKE = 'like'
+    MENTION = 'mention'
+
+    CHOICES = (
+        (MESSAGE, 'Message'),
+        (FOLLOWER, 'Follower'),
+        (LIKE, 'Like'),
+        (MENTION, 'Mention')
+    )
+
+    to_user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+
+    notification_type = models.CharField(max_length=20, choices=CHOICES)
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='creatednotifications', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
